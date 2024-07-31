c6ed20f306f49e31465ebc76916cd7b0293dbcd4
Navneet <pranavnavneet11@gmail.com>
2024-07-31 15:04:14 +0530

    updated the domain

--- frontend/src/pages/todo/Todo.jsx
+++ frontend/src/pages/todo/Todo.jsx
@@ -9,7 +9,7 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 
 function Todo() {
-  const domain = "http://localhost:8000";
+  const domain = "https://todo-1-ttrp.onrender.com";
   const [todo, setTodo] = useState([]);
   const [input, setInput] = useState("");
   const [listInput, setListInput] = useState("");
